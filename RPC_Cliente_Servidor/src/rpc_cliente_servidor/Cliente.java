153097d2ccea99b5546d213890ce3634bf869587
Jorge Luis <47648890+Jorgelbl@users.noreply.github.com>
2021-04-28 07:02:59 -0500

    RPC_Y_RMI
    Actualizacion

--- RPC_Cliente_Servidor/src/rpc_cliente_servidor/Cliente.java
+++ RPC_Cliente_Servidor/src/rpc_cliente_servidor/Cliente.java
@@ -1,21 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rpc_cliente_servidor;
 
+import java.util.Vector;
+import javax.swing.JOptionPane;
+import org.apache.xmlrpc.XmlRpcClient;
 /**
  *
  * @author Jorge
  */
 public class Cliente {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String x="", y="";
+        
+        Object resultado;
+        
+        try{
+            XmlRpcClient client = new XmlRpcClient("http://192.168.197.64:8080");
+            Vector<String> parametros = new Vector<String>();
+            
+            JOptionPane.showMessageDialog(null,"El cliente se ha conectado");
+            
+            while(true){
+                String menu = JOptionPane.showInputDialog(null, "suma de 2 numeros"
+                       + "1.-Sumar\n"
+                       + "2.-Salir\n", "Cliente Servidor RPC", JOptionPane.DEFAULT_OPTION);
+                
+                switch(menu){
+                    case "1":
+                        x = JOptionPane.showInputDialog(null, "Primer numero", "Suma", JOptionPane.QUESTION_MESSAGE);
+                        y = JOptionPane.showInputDialog(null, "Segundo numero", "Suma", JOptionPane.QUESTION_MESSAGE);
+                        
+                        parametros.addElement(x);
+                        parametros.addElement(y);
+                        
+                        resultado = client.execute("miServidorRPC.suma", parametros);
+                        
+                        JOptionPane.showMessageDialog(null, "La suma es: " + resultado);
+                        
+                        parametros.clear();
+                        
+                        break;
+                        
+                    case "2":
+                        JOptionPane.showMessageDialog(null, "Saliendo", null, JOptionPane.WARNING_MESSAGE);
+                        System.exit(0);
+                        break;      
+                }
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
     }
     
 }
